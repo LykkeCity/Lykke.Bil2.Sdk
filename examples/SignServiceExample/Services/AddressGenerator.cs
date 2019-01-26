9697a3941512981c45ce9535cee8f4ba8d5489f9
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2019-01-26 15:16:08 +0300

    Sign service example

---
+++ examples/SignServiceExample/Services/AddressGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Lykke.Blockchains.Integrations.Contract.SignService.Requests;
+using Lykke.Blockchains.Integrations.Contract.SignService.Responses;
+using Lykke.Blockchains.Integrations.Sdk;
+using Lykke.Blockchains.Integrations.Sdk.SignService.Models;
+using Lykke.Blockchains.Integrations.Sdk.SignService.Services;
+
+namespace SignServiceExample.Services
+{
+    internal class AddressGenerator : IAddressGenerator
+    {
+        public Task<AddressCreationResult> CreateAddresAsync()
+        {
+            var address = Guid.NewGuid().ToString("N");
+            var privateKey = Guid.NewGuid().ToString("N").Substring(0, 8);
+            var context = Guid.NewGuid().ToString("N").Substring(0, 4);
+
+            Console.WriteLine($"Generated address: {address}, privateKey: {privateKey}");
+
+            return Task.FromResult(new AddressCreationResult(address, privateKey, context));
+        }
+
+        public Task<CreateAddressTagResponse> CreateAddressTagAsync(string address, CreateAddressTagRequest request)
+        {
+            throw new OperationNotSupportedException("Address tag is not supported");
+        }
+    }
+}
---
+++ examples/SignServiceExample/Settings/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿using Lykke.Blockchains.Integrations.Sdk.SignService;
+
+namespace SignServiceExample.Settings
+{
+    public class AppSettings : BaseSignServiceSettings
+    {
+    }
+}
