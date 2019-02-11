8d90838ff51c3753cab91d14afdb0397b6edbb7f
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2019-02-12 01:00:15 +0300

    Blocks reader client

---
+++ src/Lykke.Blockchains.Integrations.Client.BlocksReader/BlocksReaderClientOptions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace Lykke.Blockchains.Integrations.Client.BlocksReader
+{
+    /// <summary>
+    /// Options for a blockchain integration blocks reader client.
+    /// </summary>
+    [PublicAPI]
+    public class BlocksReaderClientOptions
+    {
+        /// <summary>
+        /// Required.
+        /// RabbitMq connection string to communicate with blockchain integration blocks reader.
+        /// </summary>
+        public string RabbitMqConnString { get; set; }
+
+        /// <summary>
+        /// Required.
+        /// Parallelism of the RabbitMq message listening for each blockchain integration.
+        /// </summary>
+        public int MessageListeningParallelism { get; set; }
+
+        internal IReadOnlyCollection<string> IntegrationNames => _integrationNames;
+        
+        private List<string> _integrationNames;
+
+        internal BlocksReaderClientOptions()
+        {
+            _integrationNames = new List<string>();
+            MessageListeningParallelism = 8;
+        }
+
+        /// <summary>
+        /// Adds blockchain integration.
+        /// </summary>
+        /// <param name="integrationName"></param>
+        public void AddIntegration(string integrationName)
+        {
+            _integrationNames.Add(integrationName);
+        }
+    }
+}
---
+++ src/Lykke.Blockchains.Integrations.Client.BlocksReader/BlocksReaderClientServiceCollectionExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using JetBrains.Annotations;
+using Lykke.Blockchains.Integrations.Client.BlocksReader.Services;
+using Lykke.Blockchains.Integrations.RabbitMq;
+using Lykke.Common.Log;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lykke.Blockchains.Integrations.Client.BlocksReader
+{
+    [PublicAPI]
+    public static class BlocksReaderClientServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds blockchain integration blocks reader client services.
+        /// </summary>
+        /// <remarks>
+        /// In order to use <see cref="IBlocksReaderClient"/>, call <see cref="IBlocksReaderClient.Start"/> first
+        /// </remarks>
+        public static void AddBlocksReaderClient(
+            this ServiceCollection services,
+            Action<BlocksReaderClientOptions> configureOptions)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+            if (configureOptions == null)
+            {
+                throw new ArgumentNullException(nameof(configureOptions));
+            }
+
+            var options = new BlocksReaderClientOptions();
+
+            configureOptions.Invoke(options);
+
+            if (string.IsNullOrWhiteSpace(options.RabbitMqConnString))
+            {
+                throw new InvalidOperationException($"{nameof(options)}.{nameof(options.RabbitMqConnString)} is required.");
+            }
+            if (options.MessageListeningParallelism <= 0)
+            {
+                throw new InvalidOperationException($"{nameof(options)}.{nameof(options.MessageListeningParallelism)} should be positive number. Actual: {options.MessageListeningParallelism}");
+            }
+            if (!options.IntegrationNames.Any())
+            {
+                throw new InvalidOperationException($"{nameof(options)}.{nameof(options.IntegrationNames)} at least one integration should be registered.");
+            }
+
+            services.AddSingleton<IRabbitMqEndpoint>(s => new RabbitMqEndpoint
+            (
+                s.GetRequiredService<ILogFactory>(),
+                new Uri(options.RabbitMqConnString)
+            ));
+
+            services.AddSingleton<IBlocksReaderClient>(s => new BlocksReaderClient
+            (
+                s.GetRequiredService<ILogFactory>(),
+                s.GetRequiredService<IRabbitMqEndpoint>(),
+                s,
+                options.IntegrationNames,
+                options.MessageListeningParallelism
+            ));
+
+            services.AddTransient<IBlocksReaderApiFactory, BlocksReaderApiFactory>();
+        }
+    }
+}
