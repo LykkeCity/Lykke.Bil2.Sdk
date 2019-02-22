612bba0cd807b3c768d7c079c5c9330701a8a434
AleksandrBelousov <tortyt1@gmail.com>
2019-02-22 18:28:59 +0400

    Added base test project

---
+++ tests/Base.Tests/HttpMessageHandlers/RedirectToTestHostMessageHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Base.Tests.Extensions;
+
+namespace Base.Tests.HttpMessageHandlers
+{
+    public class RedirectToTestHostMessageHandler : HttpMessageHandler
+    {
+        private readonly HttpClient _memoryPipelineClient;
+
+        public RedirectToTestHostMessageHandler(HttpClient memoryPipelineClient)
+        {
+            _memoryPipelineClient = memoryPipelineClient;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var clonedRequest = await request.CloneAsync();
+            var response = await _memoryPipelineClient.SendAsync(clonedRequest);
+
+            return response;
+        }
+    }
+}
