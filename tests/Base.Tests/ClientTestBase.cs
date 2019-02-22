8cd105b7ddd71aeb2830bbe339344e934f611b34
AleksandrBelousov <tortyt1@gmail.com>
2019-02-22 18:28:59 +0400

    Added base test project

---
+++ tests/Base.Tests/ClientTestBase.cs
@@ -0,0 +1,26 @@
+﻿using Lykke.Sdk;
+using Microsoft.AspNetCore.TestHost;
+
+namespace Base.Tests
+{
+    public class ClientTestBase
+    {
+        public ClientTestBase()
+        {
+        }
+
+        public TestServer CreateTestServer<TStartup>() where TStartup : class
+        {
+            LykkeWebHostFactory factory = new LykkeWebHostFactory();
+            var webHostBuilder = factory.CreateWebHostBuilder<TStartup>(options =>
+            {
+                options.Port = 5000;
+                options.IsDebug = true;
+            });
+
+            TestServer testServer = new TestServer(webHostBuilder);
+
+            return testServer;
+        }
+    }
+}
