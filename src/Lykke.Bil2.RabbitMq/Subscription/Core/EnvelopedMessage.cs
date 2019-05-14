fe9e03dbc7432b15aaaab5695a0141c71912fa5d
some-dev <avolkov.dev@gmail.com>
2019-05-14 15:58:39 +0300

    add additional telemetry to enveloped message

--- src/Lykke.Bil2.RabbitMq/Subscription/Core/EnvelopedMessage.cs
+++ src/Lykke.Bil2.RabbitMq/Subscription/Core/EnvelopedMessage.cs
@@ -1,15 +1,21 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using JetBrains.Annotations;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
 
 namespace Lykke.Bil2.RabbitMq.Subscription.Core
 {
     internal sealed class EnvelopedMessage
     {
+        private static readonly TelemetryClient TelemetryClient = new TelemetryClient();
+
         private readonly IMessageConsumer _consumer;
         private readonly DateTime _createdOn;
         private readonly ulong _deliveryTag;
+        private readonly IOperationHolder<RequestTelemetry> _telemetryOperation;
 
         public EnvelopedMessage(
             IEnumerable<byte> body,
@@ -45,6 +51,8 @@ namespace Lykke.Bil2.RabbitMq.Subscription.Core
             Id = id;
             RetryCount = retryCount;
             RoutingKey = routingKey;
+
+            _telemetryOperation = TelemetryClient.StartOperation<RequestTelemetry>($"Enveloped Message: {RoutingKey}", correlationId);
         }
 
 
@@ -64,14 +72,25 @@ namespace Lykke.Bil2.RabbitMq.Subscription.Core
         
         public string RoutingKey { get; }
         
-        
         public void Ack()
         {
+            _telemetryOperation.Telemetry.ResponseCode = nameof(Ack);
+            _telemetryOperation.Telemetry.Success = true;
+            _telemetryOperation.Telemetry.Stop();
+
+            TelemetryClient.TrackRequest(_telemetryOperation.Telemetry);
+
             _consumer.Ack(_deliveryTag);
         }
 
         public void Reject()
         {
+            _telemetryOperation.Telemetry.ResponseCode = nameof(Reject);
+            _telemetryOperation.Telemetry.Success = false;
+            _telemetryOperation.Telemetry.Stop();
+
+            TelemetryClient.TrackRequest(_telemetryOperation.Telemetry);
+
             _consumer.Reject(_deliveryTag);
         }
 
@@ -79,6 +98,9 @@ namespace Lykke.Bil2.RabbitMq.Subscription.Core
         {
             var newRetryCount = RetryCount + 1;
             
+            _telemetryOperation.Telemetry.Sequence = newRetryCount.ToString();
+            TelemetryClient.TrackRequest(_telemetryOperation.Telemetry);
+
             return new EnvelopedMessage
             (
                 Body,
